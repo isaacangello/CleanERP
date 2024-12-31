3de0335f22aedd147323e9c782fd2dc0ea009be3
isaacangello <isaacangello@inf.ufel.edu.br>
2024-12-31 02:02:24 -0300

    loading modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
-    @vite(['resources/css/app.css','resources/js/app.js'])
+{{--    @vite(['resources/css/app.css','resources/js/app.js'])--}}
 
 
 
